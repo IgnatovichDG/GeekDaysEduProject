3c857a192af233806e5715a9496e8b05a5e3f180
Игнатович Даниил Григорьевич <dignatovich@team.vestbery.com>
2016-07-09 17:04:43 +0300

    Add models

---
+++ GeekDaysEdu/GeekDaysEdu/Models/CommentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeekDaysEdu.Models
+{
+    public class Comment
+    {
+        [Key]
+        public string CommentID { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime PostDate { get; set; }
+
+        public string Userid { get; set; }
+    }
+}
