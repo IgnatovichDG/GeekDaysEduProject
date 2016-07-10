195c7cbc144c49099dce6edc70820364d97f7441
Igor Tresoumov <koxrel@outlook.com>
2016-07-10 03:13:56 +0300

    List of courses for the user

---
+++ GeekDaysEdu/GeekDaysEdu/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeekDaysEdu.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GeekDaysEdu.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            List<Link> courses;
+            var userId = User.Identity.GetUserId();
+            using (var db = new Context())
+            {
+                courses = db.LinkUsersCourses
+                    .Where(l => l.UserId == userId)
+                    .Include(l => l.ResourceModel)
+                    .OrderBy(l => l.ResourceModel.Name)
+                    .ToList();
+            }
+            return View(courses);
+        }
+    }
+}
